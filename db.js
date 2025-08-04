6627a0270fafdea8a308112ff2fa3cdd4266b28c
Aakrit2211 <130543852+Aakrit2211@users.noreply.github.com>
2025-08-04 12:06:09 +0530

    db.js

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
-// db.js
-import pg from 'pg';
-const { Pool } = pg;
+import pkg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL
 });
 
-export { pool };
+export default pool;
